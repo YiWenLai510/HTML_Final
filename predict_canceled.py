d3313a4acb0816c46350041db10d027bac5d92e9
YiWenLai510 <b05901060@ntu.edu.tw>
2020-12-21 14:04:46 +0800

    predict is_canceled complete with acc about 81%

--- predict_canceled.py
+++ predict_canceled.py
@@ -7,16 +7,16 @@ class DataReader(object):
         self.train_label = self.trainRow['is_canceled']
         self.testRow = pd.read_csv(testFile)
 
-        self.not_included = self.ignored_parser(notinclude)
+        self.not_included = notinclude
         self.drop_canceled = drop_canceled
         self.not_onehot = notoneHot
 
-        self.wholeData = self.combined()
-        print(self.wholeData)
         self.trainStartIndex = 0
         self.testStartIndex = len(self.trainRow.index) # check
         self.trainEndIndex = (self.testStartIndex) - 1
-        
+
+        self.wholeData = self.combined()
+        # print(self.wholeData)
     def combined(self):
         # remove tags in train but not in test
         tmp_train = self.trainRow.drop(columns = self.not_included) # contain ID
@@ -32,99 +32,68 @@ class DataReader(object):
             if c not in self.not_onehot:
                 encode_col.append(c)
         # one hot encoder
+
         return pd.get_dummies(tmp_combined.astype(str),columns=encode_col)
-    # def add_column_to_test(self):
-    def ignored_parser(self,ignore):
-        return ignore.split()
-    def getTrain_svm(self):
-        df_to_list = self.wholeData.values.tolist()
-        # print(len(df_to_list))
-        # print(self.train_label[0])
-        # print(self.wholeData.iloc[0])
-        # print(len(df_to_list[0]))
-        with open('train_svm.txt', 'w') as f:
-            for i in range(len(df_to_list)):
-                if i <= self.trainEndIndex:
-                    row_str = ''
-                    row_str += str(self.train_label[i])
-                    for index,value in enumerate(df_to_list[i]):
-                        row_str += ' %d:%s'%(index,value)
-                    row_str += '\n'
-                    print(i)
-                    f.write(row_str)
-                else:
-                    break
-
-        # find how to return specific range of data and change to csv
-
-
-    def getTest_svm(self):
-        df_to_list = self.wholeData.values.tolist()
-        with open('test_svm.txt', 'w') as f:
-            for i in range(len(df_to_list)):
-                if i >= self.testStartIndex:
-                    row_str = ''
-                    row_str += '0'
-                    for index,value in enumerate(df_to_list[i]):
-                        row_str += ' %d:%s'%(index,value)
-                    row_str += '\n'
-                    print(i)
-                    f.write(row_str)
-
+    def add_column_to_test(self,predict_result):
+        if len(self.testRow.index) == len(predict_result):
+            self.testRow['is_canceled'] = predict_result
+            print(self.testRow)
+        else:
+            print('Error !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    def getTrainTest_svm(self):
+        df_to_numpy = self.wholeData.to_numpy()
+        return np.array(self.train_label).astype(int) ,(df_to_numpy[:self.trainEndIndex + 1]).astype(float),(df_to_numpy[self.testStartIndex:]).astype(float)
 
 def processData(train,test,notinclude,drop_canceled,notoneHot):
     df = DataReader(train,test,notinclude,drop_canceled,notoneHot)
-    df.getTrain_svm()
-    # df.getTest_svm()
-    # return df,train_svm_format,test_svm_format
+    train_y,train_x,test_x  = df.getTrainTest_svm()
+    return train_y,train_x,test_x,df
 
-def train_svm(train):
-    y, x = svm_read_problem(train)
-    model = svm_train(y, x, '-c 10 -s 0 -t 0')
+def train_svm(y,x):
+    print('train')
+    # cross valid 5
+    index_list = np.arange(x.shape[0])
+    np.random.shuffle(index_list)
+    unit_len = int(x.shape[0]/5)
+    acc = []
+    models = []
+    for i in range(5): # 5 fold cross validation
+        part_y = []
+        part_x = []
+        valid_y = []
+        valid_x = []
+        for j in range(x.shape[0]):
+            if i*unit_len <= j < ((i+1)*unit_len):
+                valid_y.append(y[index_list[j]])
+                valid_x.append(x[index_list[j]].tolist())
+            else:
+                part_y.append(y[index_list[j]])
+                part_x.append(x[index_list[j]].tolist())
+        print(i)
+        # print(len(part_y),len(part_x),len(valid_y),len(valid_x))
+        model = svm_train(np.array(part_y), np.array(part_x),'-h 0')
+        svm_save_model('is_canceled_%s.model'%(str(i)), model)
+        p_label, p_acc, p_val = svm_predict(np.array(valid_y), np.array(valid_x), model)
+        models.append(model)
+        acc.append(p_acc)
+    print('Max acc ',max(acc))
 
-    print('complete train')
-    p_label, p_acc, p_val = svm_predict(y, x, model)
-    return model
+    # check accuracy for training set
+    m_label,m_acc,m_val = svm_predict(y,x,models[acc.index(max(acc))])
 
-# def predict_and_add_iscanceled(model,test,df_obj):
-#     y, x = svm_read_problem(test)
-#     p_label, p_acc, p_val = svm_predict([], x, model) # check problem  first svm predict with no test label
-#     testDataframe = df_obj.add_column_to_test(p_label,'is_canceled')
-#     return testDataframe
+    return  models[acc.index(max(acc))]
+def predict_canceled_forTest(model,x,df):
+    label, acc, val = svm_predict([],x,model)
+    df.add_column_to_test(label)
     
 if __name__ == '__main__':
     # make train & test same dimension
-    notinclude = 'ID is_canceled adr reservation_status reservation_status_date'
+    notinclude = ['ID', 'is_canceled' ,'adr' ,'reservation_status' ,'reservation_status_date']
     # should be preserved not be encode
     notoneHot =  ['lead_time','stays_in_weekend_nights','stays_in_week_nights','adults','children','babies']
     # redunancy info for predict canceled
-    drop_canceled = ['agent', 'arrival_date_month', 'country', 'arrival_date_week_number', 'arrival_date_day_of_month']
+    drop_canceled = ['agent', 'arrival_date_month', 'country', 'arrival_date_week_number', 'arrival_date_day_of_month']# 'agent',
 
-    processData('train.csv','test.csv',notinclude,drop_canceled,notoneHot)
-    # m = train_svm('train_svm.txt')
-    # predict_and_add_iscanceled(m, testing, df_Obj)
-    # print(label)
-    # model = train(dataframe,label)
-    # predict(model,label)
-
-
-    # def processData(self):
-    #     self.train_dataframe = self.data.copy()
-    #     self.all_need_attributes = []
-    #     all_attributes = self.train_dataframe.columns.values
-    #     for tag in all_attributes:
-    #         if tag not in self.not_included:
-    #             self.all_need_attributes.append(tag)
-    #             if self.train_dataframe[tag].dtype == 'object':
-    #                 self.build_dict(tag)
-    #     self.train_dataframe = self.train_dataframe.drop(columns = self.not_included)
-    # def build_dict(self,tag):
-    #     a = []
-    #     d = {}
-    #     col = self.train_dataframe[tag]
-    #     for x in col:
-    #         if x not in a:
-    #             a.append(x)
-    #     for index,value in enumerate(a):
-    #         d[value] = index
-    #     self.train_dataframe[tag] = self.train_dataframe[tag].replace(d)
+    train_y, train_x, test_x, df_Obj = processData('train.csv', 'test.csv', notinclude, drop_canceled, notoneHot)
+    model = train_svm(train_y, train_x)
+    predict_canceled_forTest(model,test_x,df_Obj)
