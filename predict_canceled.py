b0d843da3d01545ad8aa551a7e1afca3013d4526
YiWenLai510 <b05901060@ntu.edu.tw>
2020-12-22 16:37:32 +0800

    start to build feature for predicting revenue

--- predict_canceled.py
+++ predict_canceled.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from libsvm.svmutil import *
+import calendar
 class DataReader(object):    
     def __init__(self,trainFile,testFile,notinclude,drop_canceled,notoneHot):
         self.trainRow = pd.read_csv(trainFile)
@@ -37,12 +38,52 @@ class DataReader(object):
     def add_column_to_test(self,predict_result):
         if len(self.testRow.index) == len(predict_result):
             self.testRow['is_canceled'] = predict_result
-            print(self.testRow)
+            # print(self.testRow)
         else:
+            print(len(predict_result))
             print('Error !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     def getTrainTest_svm(self):
         df_to_numpy = self.wholeData.to_numpy()
         return np.array(self.train_label).astype(int) ,(df_to_numpy[:self.trainEndIndex + 1]).astype(float),(df_to_numpy[self.testStartIndex:]).astype(float)
+    def get_predict_revenue_traintest(self,features):
+        # choose the columns preserved and rearrange the order of columns
+        df_train = self.trainRow[features][features]
+        df_test = self.testRow[features][features]
+        # delete columns with is_canceled = 1 and remove is_canceled
+        df_train = df_train[df_train.is_canceled != 1].drop(['is_canceled'],axis = 1)
+        df_test = df_test[df_test.is_canceled != str(1.0)].drop(['is_canceled'],axis = 1 )
+        # add total days
+        cols_to_sum = ['stays_in_weekend_nights', 'stays_in_week_nights']
+        df_train['total_days'] = df_train[cols_to_sum].sum(axis=1)
+        df_test['total_days'] = df_test[cols_to_sum].sum(axis=1)
+        df_train = df_train.drop(columns=cols_to_sum)
+        df_test = df_test.drop(columns=cols_to_sum)
+        # string month to int
+        month_dict = {  'Janauary':1,
+                        'February':2,
+                        'March':3,
+                        'April':4,
+                        'May':5,
+                        'June':6,
+                        'July':7,
+                        'August':8,
+                        'September':9,
+                        'October':10,
+                        'November':11,
+                        'December':12	}
+        df_train =  df_train.replace({'arrival_date_month':month_dict})
+        df_test = df_test.replace({'arrival_date_month':month_dict})
+        print(df_train)
+        print(df_test)
+        # add rows based on per day
+        for index, row in df_train.iterrows():
+            if index == 0:
+                print(row)
+        # generate new df, by sum of adults, children, babies, total special request
+
+        # add weekday, weekend column
+
+        # return new df
 
 def processData(train,test,notinclude,drop_canceled,notoneHot):
     df = DataReader(train,test,notinclude,drop_canceled,notoneHot)
@@ -77,15 +118,20 @@ def train_svm(y,x):
         models.append(model)
         acc.append(p_acc)
     print('Max acc ',max(acc))
-
     # check accuracy for training set
     m_label,m_acc,m_val = svm_predict(y,x,models[acc.index(max(acc))])
-
     return  models[acc.index(max(acc))]
+
 def predict_canceled_forTest(model,x,df):
     label, acc, val = svm_predict([],x,model)
+    with open('Test_is_canceled_Label.txt','w') as f:
+        f.write(' '.join(str(ele) for ele in label))
     df.add_column_to_test(label)
-    
+def predict_revenue(df,features):
+    df.get_predict_revenue_traintest(features)
+    # train_x,test_x = df.get_predict_revenue_traintest(features)
+    # train_y = pd.read_csv('train_label.csv')
+
 if __name__ == '__main__':
     # make train & test same dimension
     notinclude = ['ID', 'is_canceled' ,'adr' ,'reservation_status' ,'reservation_status_date']
@@ -95,5 +141,17 @@ if __name__ == '__main__':
     drop_canceled = ['agent', 'arrival_date_month', 'country', 'arrival_date_week_number', 'arrival_date_day_of_month']# 'agent',
 
     train_y, train_x, test_x, df_Obj = processData('train.csv', 'test.csv', notinclude, drop_canceled, notoneHot)
-    model = train_svm(train_y, train_x)
-    predict_canceled_forTest(model,test_x,df_Obj)
+    # model = train_svm(train_y, train_x)
+    # predict_canceled_forTest(model,test_x,df_Obj)
+    # model = svm_load_model('is_canceled_0.model')
+    # predict_canceled_forTest(model, test_x, df_Obj)
+    test_cancel_label = []
+    with open ('Test_is_canceled_Label.txt','r') as f:
+        line = f.readline()
+        # print(line)
+        test_cancel_label = line.split(' ')
+    df_Obj.add_column_to_test(test_cancel_label)
+
+    features = ['is_canceled','arrival_date_year', 'arrival_date_month','arrival_date_day_of_month',
+                'stays_in_weekend_nights','stays_in_week_nights','adults','children','babies','total_of_special_requests']
+    predict_revenue(df_Obj,features)
