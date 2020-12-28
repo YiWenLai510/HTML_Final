0c7c92a8197bc8f1c1faad96e5908014a950057a
YiWenLai510 <b05901060@ntu.edu.tw>
2020-12-28 19:05:23 +0800

    structure update with acc lastest

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ import predict_adr
 
 if __name__ == '__main__':
     # return numpy array
-    test_is_canceled = predict_canceled.predict_canceled()
-    test_adr = predict_adr.predict_adr()
-    # calculate adr
+    test_cancel,df_obj = predict_canceled.predict_canceled()
+    test_adr = predict_adr.predict_adr(df_obj)
+    # # calculate adr
     testRow = pd.read_csv('test.csv')
+    blank_label = pd.read_csv("test_nolabel.csv")
+    testRow['is_canceled']= test_cancel
+    testRow['adr'] = test_adr
+
+    print("Final data =================================")
+    print(testRow)
+
     testRow = testRow[testRow.is_canceled != 1].drop(['is_canceled'], axis=1)
+
+    print("cancel remove ==============================")
+    print(testRow)
+
     testRow['adr_days'] = testRow.adr * (testRow.stays_in_weekend_nights + testRow.stays_in_week_nights)
     month_dict = {'January': 1,
                   'February': 2,
@@ -30,7 +41,13 @@ if __name__ == '__main__':
     testRow["date"] = pd.to_datetime(testRow[["year", "month", "day"]])
     # print(testRow)
     df_tmp = testRow[['adr_days', 'date']]
-    print(df_tmp)
+
     d = df_tmp.groupby(["date"]).sum().sort_values("date")
-    print(d)
-    d.to_csv('result.csv',index=False,index_label=False)
+    print(df_tmp)
+
+    blank_label["label"] = (d["adr_days"].values // 10000).astype(int)
+    blank_label.loc[blank_label.label > 9, ["label"]] = 9
+    blank_label.loc[blank_label.label < 0, ["label"]] = 0
+    print(blank_label)
+
+    blank_label.to_csv('result.csv',index=False)
--- predict_adr.py
+++ predict_adr.py
@@ -1,5 +1,70 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer, PolynomialFeatures
+# from time import sleep
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA
+# from joblib import dump, load
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+
+def predict_adr(df):
+
+    drop = ["year", "month", "day", "is_canceled","reserved_room_type", "stays_in_weekend_nights", "stays_in_week_nights","date"]
+    toEncode = ["country", "market_segment", "distribution_channel", "assigned_room_type", "agent", "company",
+                "customer_type","hotel", "meal", "deposit_type"]
+    df.read_drop_encode(drop, toEncode, [])
+    df.drop_encode()
+
+    adrLabel, adrFeature, testf = df.getTrainTest_adr_pd()
+    print(adrLabel.shape)
+    print(adrFeature.shape)
+    print(testf.shape)
+    # print(adrFeature)
+
+    # pca = PCA(n_components=85).fit(adrFeature)
+    # adrFeature = pca.transform(adrFeature)
+
+    # model = make_pipeline(PolynomialFeatures(2), LinearRegression()).fit(adrFeature, adrLabel)
+    # 1.539474
+
+    # model = LinearRegression().fit(adrFeature, adrLabel)
+    # 0.618421
+
+    # model = SVR().fit(adrFeature, adrLabel)
+    # 0.776316
+
+    model = KNeighborsRegressor(n_neighbors=10).fit(adrFeature, adrLabel)
+    # 0.486842
+
+    # model = MLPRegressor(max_iter=500).fit(adrFeature, adrLabel)
+    # 0.486842
+
+    # print(model.score(adrFeature, adrLabel))
+    # _predict = model.predict(adrFeature)
+
+    # print((_predict-adrLabel).abs().mean())
+    # dump(model, 'model.joblib')
+
+    # testf = test.drop(columns=["adr", "days", "date"], axis=1)
+    #test_pca = pca.transform(testf)
+
+    return model.predict(testf)
+    # testf["profit"] = testf["adr"] * testf["days"]
+    # print(testf["profit"])
+    # test = test.sort_values("date").groupby(["date"]).sum()
+    # print(test)
+    #
+    # label = pd.read_csv("test_nolabel.csv")
+    # label["label"] = (test["profit"].values // 10000).astype(int)
+    # label.loc[label.label > 9, ["label"]] = 9
+    # label.loc[label.label < 0, ["label"]] = 0
+    # print(label)
+    #
+    # label.to_csv("label.csv", index=False)
+
 # trainRow = pd.read_csv('train.csv')
 # trainRow = trainRow[trainRow.is_canceled != 1].drop(['is_canceled'], axis=1)
 # trainRow['adr_days'] = trainRow.adr*(trainRow.stays_in_weekend_nights + trainRow.stays_in_week_nights)
