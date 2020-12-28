debdadf7c5763561c3477983f17aac4c6dbecb0f
YiWenLai510 <b05901060@ntu.edu.tw>
2020-12-28 10:31:04 +0800

    want to merge cancel

--- predict_canceled.py
+++ predict_canceled.py
@@ -106,7 +106,7 @@ class DataReader(object):
 
         
 
-def processData(train,test,notinclude,drop_canceled,notoneHot):
+def prgit pulocessData(train,test,notinclude,drop_canceled,notoneHot):
     df = DataReader(train,test,notinclude,drop_canceled,notoneHot)
     train_y,train_x,test_x  = df.getTrainTest_svm()
     return train_y,train_x,test_x,df
